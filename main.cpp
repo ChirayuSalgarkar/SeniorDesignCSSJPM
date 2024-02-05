f0616b3a64fa4f281bbcc180758f81896867e56f
Masmanlee <81195433+Masmanlee@users.noreply.github.com>
2024-02-05 16:02:49 -0500

    Add files via upload
    uploading to change on PC

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 
-#define FORCE_SENSOR_PIN 0 
+#define FORCE_SENSOR_PIN0 0 
+#define FORCE_SENSOR_PIN1 1 
+#define FORCE_SENSOR_PIN2 2 
+#define FORCE_SENSOR_PIN3 3 
+#define FORCE_SENSOR_PIN4 4 
+#define FORCE_SENSOR_PIN5 5 
+#define FORCE_SENSOR_PIN6 6 
 // put function declarations here:
-int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,11 +15,52 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int analogReading = analogRead(FORCE_SENSOR_PIN);
+  //each loop, array of 7 reading read from each GPIO pin 
+  int reading[7];
+ 
+  for(int i =0; i < 7; i++)
+  {
+    delay(100);
+    switch (i) {
+      case 0:
+        //reading[i] = analogRead(FORCE_SENSOR_PIN0);
+        Serial.write(analogRead(FORCE_SENSOR_PIN0));
+        break;
 
-  Serial.print("The force sensor value = ");
-  Serial.print(analogReading); // print the raw analog reading
+      case 1:
+        //reading[i] = analogRead(FORCE_SENSOR_PIN1);
+        Serial.write("S1:" + analogRead(FORCE_SENSOR_PIN1));
+        break;
+      
+      case 2:
+        Serial.write("S1:" + analogRead(FORCE_SENSOR_PIN2));
+        //reading[i] = analogRead(FORCE_SENSOR_PIN2);
+        break;
+
+       case 3:
+        // reading[i] = analogRead(FORCE_SENSOR_PIN3);
+        Serial.write("S1:" + analogRead(FORCE_SENSOR_PIN3));
+        break;
+
+      case 4:
+        // reading[i] = analogRead(FORCE_SENSOR_PIN4);
+        Serial.write("S1:" + analogRead(FORCE_SENSOR_PIN4));
+        break;
+      
+      case 5:
+        // reading[i] = analogRead(FORCE_SENSOR_PIN5);
+        Serial.write("S1:" + analogRead(FORCE_SENSOR_PIN5));
+        break;
+
+      case 6:
+        // reading[i] = analogRead(FORCE_SENSOR_PIN6);
+        Serial.write("S1:" + analogRead(FORCE_SENSOR_PIN6));
+        break;
+    }
+
+    
+    
+  }
 }
 
 // put function definitions here:
